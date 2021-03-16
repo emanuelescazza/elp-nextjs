d218951c185a3f2913e81b78c9bd5c66f30dfd23
Manu <manu.scazza@hotmail.it>
2021-03-16 11:37:35 +0100

    layout component with head

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,26 @@
-import styles from '../styles/Layout.module.css'
+import styles from '../styles/Layout.module.css';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-export default function Layout({ children }) {
+export default function Layout({ children, pageTitle, description, ...props }) {
+  const router = useRouter();
+  console.log(`${router.basePath}${router.pathname}`);
   return (
-    <div className={styles.content}>{children}</div>
-  );
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} key="ogtitle" />
+        <meta property="og:description" content={description} key="ogdesc" />
+        <meta property="og:site_name" content={'elp!'} key="ogsitename" />
+        <meta property="og:url" content={`${router.basePath}${router.pathname}`} key="ogurl" />
+      </Head>
+      <section>
+        <div className={styles.content}>{children}</div>
+      </section>
+    </>
+  )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,9 +11,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <div id="page-container">
       <Head>
-        <title>Studio Alessia Pompamea</title>
+        <title>elp!</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="description" content={'Empatia Linguaggio Pragmatica'} />
+        <meta property="og:title" content="elp!" key="ogtitle" />
+        <meta property="og:description" content={'Empatia Linguaggio Pragmatica'} key="ogdesc" />
+        <meta property="og:site_name" content={'elp!'} key="ogsitename" />
+        <meta property="og:url" content={'currentURL'} key="ogurl" />
       </Head>
       <div id="content-wrap">
         <Navbar />
