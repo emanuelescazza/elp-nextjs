04d708f25d73efe031c4bd0f0450c0a5aae0547d
Manu <manu.scazza@hotmail.it>
2021-03-20 16:26:55 +0100

    change article img

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children, pageTitle, description, article, ...p
     <>
       <Head>
         <title>{pageTitle}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon2.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
